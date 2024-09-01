ec0a6080b39e9047e8dca6753eb32d7262db9e96
DongMin <min4263@hotmail.com>
2024-09-01 22:50:35 +0900

    2028/09/01

---
+++ src/baekjoon/baekjoon_15439.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon_15439 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		
+		System.out.println(N*(N-1));
+		
+		
+	}
+
+}
---
+++ src/baekjoon/baekjoon_24723.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon_24723 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		
+		System.out.println((int)Math.pow(2, N));
+		
+	}
+
+}
